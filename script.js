b70b5d55eb0c923aaa136abedda089e4fc2f0837
adamglupsk <124837683+adamglupsk@users.noreply.github.com>
2023-02-17 10:42:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+(function() {
+  const interval = 1000;
+  const timeout = 60;
+  let idleCounter = 0;
+  window.onload = document.onmousemove = document.onkeypress = function() {
+    idleCounter = 0;
+    document.documentElement.classList.remove('idle');
+  };
+  window.setInterval(function() {
+    if (++idleCounter >= timeout) {
+      document.documentElement.classList.add('idle');
+      idleCounter = 0;
+    }
+  }, interval);
+})();
+
+
+function visaSvar() {
+  event.preventDefault();
+  const allaSvar = ["index.html","websave.html"];
+  const randomSvar = allaSvar[getRandomInt(allaSvar.length)];
+  location.assign(randomSvar);
+}
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
